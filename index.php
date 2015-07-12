4cefe9416e487f648407435d38b23c1cc527effe
Chris Birchall <chris.birchall@gmail.com>
2015-07-12 18:32:18 +0100

    Version 2

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <html>
- <head>
-  <title>My legacy PHP app</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?> 
- </body>
+  <head>
+    <title>My legacy PHP app</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello version 2</p>'; ?>
+  </body>
 </html>
---
+++ release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+version=${1?"Version not specified"}
+
+sed -i "" -e "s/Hello version [0-9]*/Hello version $version/" index.php
+
+git add .
+git commit -m "Version $version"
+git tag "v$version"
+#git push --tags origin master
